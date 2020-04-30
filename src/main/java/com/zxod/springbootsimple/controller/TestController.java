630bce4dd5413d7c17145bcb32f10b322957af87
zxod <zxod@foxmail.com>
2020-04-30 18:03:39 +0800

    update(sqlMapper): 更新sql mapper

--- src/main/java/com/zxod/springbootsimple/controller/TestController.java
+++ src/main/java/com/zxod/springbootsimple/controller/TestController.java
@@ -8,7 +8,10 @@ import com.zxod.springbootsimple.module.Hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,5 +40,27 @@ public class TestController {
             scrawlRuleMapper.getAll(),
             sscrawlRuleMapper.getAll()
         );
+    }
+
+    @PostMapping("/scrawl_rules")
+    public Object createScrawlRules(
+        @RequestParam("name") String name,
+        @RequestParam("description") String description,
+        @RequestParam("homeUrl") String homeUrl,
+        @RequestParam("pageUrl") String pageUrl,
+        @RequestParam("rule") String rule,
+        @RequestParam("creator") String creator
+    ) {
+        scrawlRuleMapper.insert(name, description, homeUrl, pageUrl, rule, creator);
+        return null;
+    }
+
+    @PatchMapping("/scrawl_rules")
+    public Object updateScrawlRules(
+        @RequestParam("id") Integer id,
+        @RequestParam("rule") String rule
+    ) {
+        scrawlRuleMapper.updateRuleById(id, rule);
+        return null;
     }
 }
--- src/main/java/com/zxod/springbootsimple/mapper/test/ScrawlRuleMapper.java
+++ src/main/java/com/zxod/springbootsimple/mapper/test/ScrawlRuleMapper.java
@@ -4,11 +4,19 @@ import java.util.List;
 
 import com.zxod.springbootsimple.entity.ScrawlRule;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface ScrawlRuleMapper {
     @Select("select * from scrawl_rule")
     List<ScrawlRule> getAll();
+
+    @Insert("insert into scrawl_rule (name, description, home_url, page_url, rule, creator) values (#{name,jdbcType=VARCHAR}, #{description,jdbcType=VARCHAR}, #{homeUrl,jdbcType=VARCHAR}, #{pageUrl,jdbcType=VARCHAR}, #{rule,jdbcType=VARCHAR}, #{creator,jdbcType=VARCHAR});")
+    void insert(String name, String description, String homeUrl, String pageUrl, String rule, String creator);
+
+    @Update("update scrawl_rule set rule = #{rule,jdbcType=VARCHAR} where id = #{id, jdbcType=INTEGER}")
+    void updateRuleById(Integer id, String rule);
 }
